7304a323c04e1a6940467d8a375a3e55e755b00a
Emmanuel Ajayi <emmanuelajayi@Emmanuels-MacBook-Pro.local>
2023-11-09 11:53:16 +0000

    different feature

--- github_branching_demo/src/main/java/Main.java
+++ github_branching_demo/src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("We're writing this on Develop");
+        System.out.println("Editing Line 6 on a different branch");
     }
 }
